a9fb2b4cf0be26815a07d2b712eee3f860347b19
Regis Michael <regis.michael101@gmail.com>
2025-02-23 21:25:05 -0300

    funcionalidades novas

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,12 @@
 
 public class Main {
     public static void main(String[] args) {
-        User admin = new User("Regis", true, "2356");
-        User client = new User("Cliente", false, "1234");
-
-
-        showSalesMenu();
-        showLoginMenu();
-         //FileService.writeFile(Products.getPathFileProduct());
-         //FileService.readFile(Products.getPathFileProduct());
+        Menus.showLoginMenu();
+         // FileService.writeFile(Products.getPathFileProduct());
+         // FileService.readFile(Products.getPathFileProduct());
     }
 
-    static private void showSalesMenu() {
-        JOptionPaneFunctions.menuSales();
-    }
 
-    static private void showLoginMenu() {
-        JOptionPaneFunctions.menuUsers();
-    }
 
 
 
